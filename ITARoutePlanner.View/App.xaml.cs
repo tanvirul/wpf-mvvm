cd6c980202038ab6657cf2442e0f85dbf35fc441
Tanvirul Islam <tanvirul1.islam@outlook.com>
2022-10-19 23:59:41 +0600

    first commit

---
+++ ITARoutePlanner.View/App.xaml.cs
@@ -0,0 +1,57 @@
+﻿using ITARoutePlanner.View.HostBuilders;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ITARoutePlanner.View
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly IHost _host;
+        public App()
+        {
+            _host = CreateHostBuilder().Build();
+        }
+        public static IHostBuilder CreateHostBuilder(string[] args = null)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .AddConfiguration()
+                .AddDbContext()
+                .AddServices()
+                .AddStores()
+                .AddViewModels()
+                .AddViews();
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _host.Start();
+
+            //SimpleTraderDbContextFactory contextFactory = _host.Services.GetRequiredService<SimpleTraderDbContextFactory>();
+            //using (SimpleTraderDbContext context = contextFactory.CreateDbContext())
+            //{
+            //    context.Database.Migrate();
+            //}
+
+            //Window window = _host.Services.GetRequiredService<MainWindow>();
+            //window.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await _host.StopAsync();
+            _host.Dispose();
+
+            base.OnExit(e);
+        }
+    }
+}
---
+++ ITARoutePlanner.View/HostBuilders/AddDbContextHostBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITARoutePlanner.View.HostBuilders
+{
+    public static class AddDbContextHostBuilderExtensions
+    {
+        public static IHostBuilder AddDbContext(this IHostBuilder host)
+        {
+            host.ConfigureServices((context, services) =>
+            {
+                string connectionString = context.Configuration.GetConnectionString("sqlite");
+                Action<DbContextOptionsBuilder> configureDbContext = o => o.UseSqlite(connectionString);
+
+                //services.AddDbContext<SimpleTraderDbContext>(configureDbContext);
+                //services.AddSingleton<SimpleTraderDbContextFactory>(new SimpleTraderDbContextFactory(configureDbContext));
+            });
+
+            return host;
+        }
+    }
+}
